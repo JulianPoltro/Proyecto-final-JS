ef5c3728b06a88c1707f999496e0a272f3a2783a
Julian Poltronieri <JulianPoltronieri@hotmail.com>
2023-10-14 17:35:32 -0300

    creando repo

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const $ = document
+const id = (id)=> $.getElementById(id)
+const selectorAll = (selector) => $.querySelectorAll(selector)
+
+const section_products = id('products')
+
+
+
+fetch('../data.json',{method:'GET'})
+    .then((response) => response.json())
+    .then((products) => console.log(products))
