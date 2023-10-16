af9ed981210287b1a5b0fd085116065826dffc4f
Julian Poltronieri <JulianPoltronieri@hotmail.com>
2023-10-16 12:41:05 -0300

    acomodando cards

--- js/funciones.js
+++ js/funciones.js
@@ -2,9 +2,9 @@ export const crearCards = (array,contenedor) => {
     array.map(item => {
         contenedor.innerHTML += `
         <article class="productCard">
-            <img src="./assets/img/vanitory1.jpg" alt="">
-            <a href="#">Vanitory</a>
-            <span>$100</span>
+            <img src="${item.imagen}" alt="${item.etiquetas}">
+            <a href="#">${item.titulo}</a>
+            <span>${item.precio}</span>
         </article>
         `
     })
