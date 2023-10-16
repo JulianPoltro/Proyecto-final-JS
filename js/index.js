feeb4046f406c91b51346e40a15de92b1c95115b
Julian Poltronieri <JulianPoltronieri@hotmail.com>
2023-10-16 17:34:43 -0300

    cantidad por select

--- js/index.js
+++ js/index.js
@@ -3,10 +3,14 @@ import { obtenerProductos } from "./funciones.js"
 
 const $ = document
 const id = (id) => $.getElementById(id)
-const selectorAll = (selector) => $.querySelectorAll(selector)
+const selector = (selector) => $.querySelector(selector)
 
 const section_products = id('products')
 
-const cantCards = id('cantidadSelect')
-
-obtenerProductos("../data.json",section_products, cantCards.value);
+// const selectCantidad = selector('#cantidadSelect')
+// selectCantidad.addEventListener("change", (e,cant) => {
+//     e.preventDefault();
+//     cant = e.target.value;
+//     obtenerProductos("../data.json", section_products, cant);
+// })
+obtenerProductos("../data.json", section_products, 8);
