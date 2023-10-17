06e69ad442d3353fae837b57004033e1ab3cd0d8
Julian Poltronieri <JulianPoltronieri@hotmail.com>
2023-10-17 18:07:54 -0300

    ref link

---
+++ js/carrito.js
@@ -0,0 +1,7 @@
+import { crearCards ,obtenerProductos} from "./funciones.js"
+
+
+const $ = document
+const id = (id) => $.getElementById(id)
+const selectorAll = (selector) => $.querySelectorAll(selector)
+
--- js/index.js
+++ js/index.js
@@ -4,15 +4,20 @@ import { crearCards ,obtenerProductos} from "./funciones.js"
 const $ = document
 const id = (id) => $.getElementById(id)
 const selector = (selector) => $.querySelector(selector)
+const selectorAll = (selectorAll) => $.querySelectorAll(selectorAll)
 
 const section_productsCat = id('productsCategory')
-
+let btnCarrito = selectorAll(".cardBtn")
 
 obtenerProductos("../data.json", section_productsCat);
 
 
 
+console.log(btnCarrito)
 
+const actualizarBtnCarrito = () =>{
+    btnCarrito = document.querySelectorAll(".cardBtn");
+}
 
 
 
