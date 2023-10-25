5cedc15c5eb7eda336e54e9be21be508de08c1d9
Julian Poltronieri <JulianPoltronieri@hotmail.com>
2023-10-25 01:20:27 -0300

    sweetalert

--- js/detalle.js
+++ js/detalle.js
@@ -1,18 +1,46 @@
-
+import { actualizarBtnCarrito } from "./actualizarCarrito.js";
 
 
 
 export const mostrarDetalle = () => {
+
+
     const detalleCards = document.querySelectorAll(".cardBtnDetail");
-    
+
     detalleCards.forEach(card => {
         card.addEventListener('click', (e) => {
-            e.preventDefault();
-            Swal.fire({
-                title: "",
-                text: "Contenido",
-                icon: 'info'
-            });
+
+            fetch('../data.json')
+                .then((response) => response.json())
+                .then((data) => {
+
+                    e.preventDefault();
+                    const idDetalle = e.target.id.split("detalle-")[1]
+                    const cardDetalle = data.find(datos => datos.id == idDetalle);
+                    Swal.fire({
+                        title: cardDetalle.titulo,
+                        text: cardDetalle.descripcion,
+                        text: cardDetalle.precio,
+                        imageUrl: cardDetalle.imagen,
+                        imageWidth: 200,
+                        imageHeight: 200,
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Agregar al carrito!',
+                        cancelButtonText: 'Seguir comprando',
+                        preConfirm: () => actualizarBtnCarrito()
+                    // }).then((result) => {
+                    //     if (result.isConfirmed) {
+                    //         Swal.fire(
+                    //             'Gracias!',
+                    //             'El producto ha sido agregado con exito.',
+                    //             'success',
+                    //         )
+
+                    //     }
+                    })
+                });
         });
     });
 }
